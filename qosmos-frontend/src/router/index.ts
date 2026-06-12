24348f774a42c5ecef6afb85abce895b6613e6bf
Benja5225 <benjamin.fuentealba.poblete@alumnos.uta.cl>
2026-06-12 03:13:49 -0400

    Implementacion de menu principal, simulador de 2 qubits y modulo interactivo de doble rendija con fasores y electrones

--- qosmos-frontend/src/router/index.ts
+++ qosmos-frontend/src/router/index.ts
@@ -2,7 +2,23 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: () => import('../views/HomeView.vue')
+    },
+    {
+      path: '/simulador-2q',
+      name: 'simulator-2q',
+      component: () => import('../views/Simulator2Q.vue')
+    },
+    {
+      path: '/simulador-rendija',
+      name: 'simulator-rendija',
+      component: () => import('../views/DoubleSlitView.vue')
+    }
+  ],
 })
 
 export default router
